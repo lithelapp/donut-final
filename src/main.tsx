e02c14fb534564e2db9babedaeafcac6c615ce8e
Eldric <lithelapp@gmail.com>
2025-06-02 13:50:45 +0100

    clean up

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// https://reactrouter.com/6.30.1/start/tutorial
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
